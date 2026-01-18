61ca030fe664816d51aedd677b0d4fca88a7b2f4
Anirudh Boreddy <anirudh.boreddy@masonrobotics.org>
2026-01-18 14:17:06 -0500

    troubleshooting

--- Auto_learning/src/main.py
+++ Auto_learning/src/main.py
@@ -9,21 +9,24 @@
 
 # Library imports
 from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+lm=Motor(Ports.PORT7, 1.0, True)
+rm=Motor(Ports.PORT12, 1.0, False)
+drivetrain=DriveTrain(lm,rm)
+
+claw_motor = Motor(Ports.PORT4)
+lift_motor = Motor(Ports.PORT11)    
+
+brain.screen.print("Hello IQ2")
+
 def main():
-    # Brain should be defined by default
-    brain=Brain()
-    lm=Motor(Ports.PORT7, 1.0, True)
-    rm=Motor(Ports.PORT12, 1.0, False)
-    drivetrain=DriveTrain(lm,rm)
-
-    claw_motor = Motor(Ports.PORT4)
-    lift_motor = Motor(Ports.PORT11)    
-
-    brain.screen.print("Hello IQ2")
     drivetrain.drive_for(FORWARD, 350, MM)
     drivetrain.turn_for(RIGHT, 45, DEGREES)
     lift_motor.spin_for(REVERSE, 360, DEGREES)
     drivetrain.drive_for(FORWARD, 110, MM)
+    print("Milestone 1")
     lift_motor.spin_for(FORWARD, 450, DEGREES)
     claw_motor.spin_for(FORWARD, 360, DEGREES)
     drivetrain.drive_for(REVERSE, 50, MM)
@@ -45,11 +48,13 @@ def main():
     claw_motor.spin_for(REVERSE, 360, DEGREES)
     lift_motor.spin_for(REVERSE, 110, DEGREES)
     lift_motor.spin_for(FORWARD, 500, DEGREES)
+    print("Milestone 2")
     claw_motor.spin_for(FORWARD, 360, DEGREES)
     drivetrain.drive_for(REVERSE, 250, MM)
     drivetrain.turn_for(LEFT, 90, DEGREES)
     drivetrain.drive_for(FORWARD, 400, MM)
     drivetrain.turn_for(RIGHT, 90, DEGREES)
+    brain.screen.new_line()
+    brain.screen.print("DONE")
 
-
-   
+auton_thread = Thread(main)
